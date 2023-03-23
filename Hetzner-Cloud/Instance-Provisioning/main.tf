9795fb1f76c2c725b500c75e9a31778383ef15a0
fiveraptor <jorisbieg@gmail.com>
2023-03-23 22:09:22 +0100

    created provisioning

---
+++ Hetzner-Cloud/Instance-Provisioning/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.36.2"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  sensitive = true
+}
+
+provider "hcloud" {
+  # Configuration options
+  token = var.hcloud_token
+}
+
+# Create a new SSH key
+resource "hcloud_ssh_key" "default" {
+  name = "station"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Create node1 with following options
+resource "hcloud_server" "debian" {
+  name = "debian"
+  image = "debian-11"
+  server_type = "cx11"
+  location = "nbg1"
+  ssh_keys  = [
+  "station"
+  ]
+    public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+
+}
